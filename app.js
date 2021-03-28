3e430c354ca3b82786b5210943afda9fd23970f5
Pedro Barros <pedroluisbarros@yahoo.com>
2021-03-29 00:02:10 +0100

    minor changer for heroku

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const crypto = require('crypto');
 
 
-app.listen(7777, () => console.log("Server started at port 7777"))
+app.listen(3000, () => console.log("Server started at port 3000"))
 app.use(express.static('public'));
 
 
